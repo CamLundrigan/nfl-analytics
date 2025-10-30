ea2434945e4cc333304aa39eed67681fb1e7c261
CamLundrigan <calundrigan@mun.ca>
2025-10-30 09:17:07 -0230

    Changed database to only include this years games

--- src/ingest/load_data.py
+++ src/ingest/load_data.py
@@ -2,11 +2,13 @@ import nflreadpy as nfl
 import pandas as pd
 import os
 from sqlalchemy import create_engine
+import datetime as dt
 from dotenv import load_dotenv
 load_dotenv()  # loads .env from the current working directory
 # Load current season play-by-play data
-pbp = nfl.load_pbp().to_pandas()
-print(pbp.columns.tolist())
+year=dt.date.today().year
+pbp = nfl.load_pbp(year).to_pandas()
+
 
 # Build SQLAlchemy engine from environment
 pg_user = os.getenv("POSTGRES_USER")
@@ -14,6 +16,9 @@ pg_pass = os.getenv("POSTGRES_PASSWORD")
 pg_host = os.getenv("POSTGRES_HOST")
 pg_port = os.getenv("POSTGRES_PORT", "5432")
 pg_db   = os.getenv("POSTGRES_DB")
+
+
+
 
 engine = create_engine(
     f"postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
