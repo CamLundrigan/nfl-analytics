d776482020790dddc6cbbe4736cff2ddf63ea622
CamLundrigan <calundrigan@mun.ca>
2025-10-30 08:52:21 -0230

    Placed database info in the .env file, changed load_data.py to acess this info using os and then uploaded data to the postgreSQL database utilizing the create_engine function by sqlalchemy

--- src/ingest/load_data.py
+++ src/ingest/load_data.py
@@ -1,21 +1,25 @@
 import nflreadpy as nfl
 import pandas as pd
-import psycopg2
+import os
 from sqlalchemy import create_engine
-
+from dotenv import load_dotenv
+load_dotenv()  # loads .env from the current working directory
 # Load current season play-by-play data
 pbp = nfl.load_pbp().to_pandas()
 print(pbp.columns.tolist())
 
-# Create connection to PostgreSQL
-conn = psycopg2.connect(
-    host="localhost",
-    database="NFL-STATS",
-    user="postgres",
-    password="STINKY"
+# Build SQLAlchemy engine from environment
+pg_user = os.getenv("POSTGRES_USER")
+pg_pass = os.getenv("POSTGRES_PASSWORD")
+pg_host = os.getenv("POSTGRES_HOST")
+pg_port = os.getenv("POSTGRES_PORT", "5432")
+pg_db   = os.getenv("POSTGRES_DB")
+
+engine = create_engine(
+    f"postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
 )
 
 # Upload to database
-pbp.to_sql('nfl_pbp', conn, if_exists='replace', index=False)
+pbp.to_sql('nfl_pbp', engine, if_exists='replace', index=False)
 
 print("Data loaded successfully")
