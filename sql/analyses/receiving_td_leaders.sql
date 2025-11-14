852c64d96ecd81e5fb24e026175a477fa2722b02
CamLundrigan <calundrigan@mun.ca>
2025-11-13 22:28:32 -0330

    Fixed error where different players with the same first name initial and last name have their totals combined instead of seperated

--- sql/analyses/receiving_td_leaders.sql
+++ sql/analyses/receiving_td_leaders.sql
@@ -1,6 +1,6 @@
-SELECT "receiver_player_name" AS player, COUNT(*) AS receiving_tds
+SELECT "receiver_player_name" AS player, "receiver_player_id" AS player_id, COUNT(*) AS receiving_tds
 FROM nfl_pbp
 WHERE "pass_touchdown" = 1 AND pass=1 -- Use the actual passing TD column
-GROUP BY "receiver_player_name"
+GROUP BY "receiver_player_name", "receiver_player_id"
 HAVING COUNT(*) > 0  -- Use COUNT(*) instead of the alias, due to sql evaluation order
 ORDER BY receiving_tds DESC;
